156b633d9e73f5d9910b0c6285511aef8e127117
kumari tanushree <ktanushree@vmware.com>
2023-04-25 23:00:21 +0530

    Added apps status check to wait until app reconcile succeeded

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	kcv1alpha1 "github.com/vmware-tanzu/carvel-kapp-controller/pkg/apis/kappctrl/v1alpha1"
 	"github.com/vmware-tanzu/carvel-kapp-controller/pkg/client/clientset/versioned"
 	kcexternalversions "github.com/vmware-tanzu/carvel-kapp-controller/pkg/client/informers/externalversions"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
@@ -40,8 +41,8 @@ var installationDetail = result{
 
 func main() {
 
-	fmt.Printf("\n**** start script ***\n")
-	config, err := clientcmd.BuildConfigFromFlags("", "/Users/ktanushree/.kube/config")
+	fmt.Printf("\n**** Collecting apps installation time... ***\n")
+	config, err := clientcmd.BuildConfigFromFlags("", "/Users/ktanushree/.kube/config") // need to change it to take from user
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -59,9 +60,9 @@ func main() {
 		go sharedInformer(clientset, appName, wg)
 	}
 	wg.Wait()
-	// installationDetail.lock.Lock()
-	// fmt.Printf("\nResult: %+v\n", installationDetail.detail)
-	// installationDetail.lock.Unlock()
+	installationDetail.lock.Lock()
+	fmt.Printf("\nResult-final: %+v\n", installationDetail.detail)
+	installationDetail.lock.Unlock()
 }
 
 type handler struct {
@@ -88,7 +89,6 @@ func sharedInformer(clientset *versioned.Clientset, appName string, wg *sync.Wai
 		UpdateFunc: h.udpateEventHandler,
 	})
 
-	//fmt.Printf("\n**** start go routine ***\n")
 	go informer.Run(h.stopperChan)
 
 	if !cache.WaitForCacheSync(stopperChan, informer.HasSynced) {
@@ -103,37 +103,121 @@ func sharedInformer(clientset *versioned.Clientset, appName string, wg *sync.Wai
 	}
 }
 
+type AppStatusDiff struct {
+	old                  kcv1alpha1.AppStatus
+	new                  kcv1alpha1.AppStatus
+	lastSeenDeployStdout string
+}
+
 func (h *handler) udpateEventHandler(oldObj interface{}, newObj interface{}) {
 
 	newApp, _ := newObj.(*kcv1alpha1.App)
-	//oldApp, _ := newObj.(*kcv1alpha1.App)
-	//fmt.Printf("\n\nNew app creation time: [%+v]\n\n", newApp.CreationTimestamp.Time)
+	oldApp, _ := oldObj.(*kcv1alpha1.App)
 
 	installationDetail.lock.Lock()
+	defer installationDetail.lock.Unlock()
 	if _, found := installationDetail.detail[newApp.Name]; !found {
 		if newApp.Status.ConsecutiveReconcileSuccesses == 1 {
 			installationDetail.detail[newApp.Name] = appDetails{
 				name:          newApp.Name,
 				namespace:     newApp.Namespace,
-				creationTime:  newApp.CreationTimestamp.String(),
-				fetchTime:     newApp.Status.Fetch.StartedAt.String(),
-				collectedTime: time.Now().Local().String(),
+				creationTime:  newApp.CreationTimestamp.UTC().String(),
+				fetchTime:     newApp.Status.Fetch.StartedAt.UTC().String(),
+				collectedTime: time.Now().UTC().String(),
 			}
 
 		}
 	}
-	fmt.Printf("\nResult: %+v\n", installationDetail.detail)
-	installationDetail.lock.Unlock()
+	//fmt.Printf("\nResult: %+v\n", installationDetail.detail)
 
-	// stopWatch, deployOutput, err := app.NewAppStatusDiff(oldApp.Status, newApp.Status, h.statusUI, h.lastSeenDeployStdout).PrintUpdate()
-	// h.lastSeenDeployStdout = deployOutput
-	// h.watchError = err
-	// if stopWatch {
-	// 	h.stopWatch()
-	// }
-	h.stopWatch()
+	appStatus := AppStatusDiff{
+		old: oldApp.Status,
+		new: newApp.Status,
+	}
+
+	stopWatch, err := appStatus.CheckAppStatus()
+	if err != nil {
+		fmt.Errorf("Error: %s\n", err.Error())
+	}
+
+	if stopWatch {
+		h.stopWatch()
+	}
 }
 
 func (h *handler) stopWatch() {
 	close(h.stopperChan)
+}
+
+// Check apps status
+func (d *AppStatusDiff) CheckAppStatus() (bool, error) {
+	if d.new.Fetch != nil {
+		if d.old.Fetch == nil || !d.old.Fetch.UpdatedAt.Equal(&d.new.Fetch.UpdatedAt) {
+			if d.new.Fetch.ExitCode != 0 && d.new.Fetch.UpdatedAt.Unix() >= d.new.Fetch.StartedAt.Unix() {
+				msg := "Fetch failed"
+				return true, fmt.Errorf(msg)
+			}
+		}
+	}
+	if d.new.Template != nil {
+		if d.old.Template == nil || !d.old.Template.UpdatedAt.Equal(&d.new.Template.UpdatedAt) {
+			if d.new.Template.ExitCode != 0 {
+				msg := "Template failed"
+				return true, fmt.Errorf(msg)
+			}
+		}
+	}
+	if d.new.Deploy != nil {
+		isDeleting := IsDeleting(d.new)
+		ongoingOp := "Deploy"
+		if isDeleting {
+			ongoingOp = "Delete"
+		}
+
+		if d.old.Deploy == nil || !d.old.Deploy.UpdatedAt.Equal(&d.new.Deploy.UpdatedAt) {
+			if d.new.Deploy.ExitCode != 0 && d.new.Deploy.Finished {
+				msg := fmt.Sprintf("%s failed", ongoingOp)
+				return true, fmt.Errorf(msg)
+			}
+		}
+	}
+
+	if HasReconciled(d.new) {
+		return true, nil
+	}
+	failed, errMsg := HasFailed(d.new)
+	if failed {
+		return true, fmt.Errorf(errMsg)
+	}
+	return false, nil
+}
+
+func HasReconciled(status kcv1alpha1.AppStatus) bool {
+	for _, condition := range status.Conditions {
+		if condition.Type == kcv1alpha1.ReconcileSucceeded && condition.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
+
+func HasFailed(status kcv1alpha1.AppStatus) (bool, string) {
+	for _, condition := range status.Conditions {
+		if condition.Type == kcv1alpha1.ReconcileFailed && condition.Status == corev1.ConditionTrue {
+			return true, fmt.Sprintf("%s: %s", kcv1alpha1.ReconcileFailed, status.UsefulErrorMessage)
+		}
+		if condition.Type == kcv1alpha1.DeleteFailed && condition.Status == corev1.ConditionTrue {
+			return true, fmt.Sprintf("%s: %s", kcv1alpha1.DeleteFailed, status.UsefulErrorMessage)
+		}
+	}
+	return false, ""
+}
+
+func IsDeleting(status kcv1alpha1.AppStatus) bool {
+	for _, condition := range status.Conditions {
+		if condition.Type == kcv1alpha1.Deleting && condition.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
 }
